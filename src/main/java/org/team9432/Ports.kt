85e8a4f373d38b58232341ad8a7a97979df0e419
nolan <nolan.s.locke@gmail.com>
2023-10-12 00:03:49 -0700

    Add remaining motor and sensor ids

--- src/main/java/org/team9432/Ports.kt
+++ src/main/java/org/team9432/Ports.kt
@@ -1,6 +1,13 @@
 package org.team9432
 
 object Ports {
+    // Motors
     const val LEFT_DRIVE_ID = 1
     const val RIGHT_DRIVE_ID = 2
+    const val SHOOTER_ID = 3
+    const val LOADER_ID = 4
+    const val HOOD_ID = 5
+
+    // Sensors
+    const val HOOD_LIMIT_PORT = 0
 }
